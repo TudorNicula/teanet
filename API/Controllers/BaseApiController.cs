1ee06d643d0039b31dcf45ac39153427f14a31c4
tudor-nicula <tudor.nicula@ops8.io>
2025-01-17 14:33:56 +0200

    Sort and Filter

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+using System;
+using API.RequestHelpers;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    protected async Task<ActionResult> CreatePagedResult<T>(IGenericRepository<T> repo, ISpecification<T> spec, int pageIndex, int pageSize) where T : BaseEntity
+    {
+        var item = await repo.ListAsync(spec);
+        var count = await repo.CountAsync(spec);
+
+        var pagination = new Pagination<T>(pageIndex, pageSize, count, item);
+
+        return Ok(pagination);  
+    }
+}
