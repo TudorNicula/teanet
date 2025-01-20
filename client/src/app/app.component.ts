1d58572e27cc457f41200941b0d66383042499ef
tudor-nicula <tudor.nicula@ops8.io>
2025-01-20 16:18:57 +0200

    Sort/filter/search

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./layout/layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-  title = 'Teanet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-      this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-        next: response => this.products = response.data,
-        error: error => console.error(error),
-        complete: () => console.log('complete')
-      });
-  }
-
+export class AppComponent {
+  title = 'Teanet'
 }
