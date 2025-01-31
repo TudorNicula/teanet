bb20f5d6aa7eff35a748765c6796cbccdd1b8ad0
tudor-nicula <tudor.nicula@ops8.io>
2025-01-31 13:25:35 +0200

    API & Client Checkout + Stripe Payment

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51QlojTDGz1GnCI2KYh2sOP8KkrXPwzYIwqpzi25JfGX7KzsgGEuTohv1Ify1LXREnp8lmLo0meHVTEncmvUrzf5B00lYrqEGR6'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51QlojTDGz1GnCI2KYh2sOP8KkrXPwzYIwqpzi25JfGX7KzsgGEuTohv1Ify1LXREnp8lmLo0meHVTEncmvUrzf5B00lYrqEGR6'
 };
