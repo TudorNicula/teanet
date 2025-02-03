7beafc44ee20250b525b322ff4065418c63ca3d2
tudor-nicula <tudor.nicula@ops8.io>
2025-02-03 14:03:39 +0200

    API Orders

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,7 +7,7 @@ namespace API.Controllers;
 
 
 
-public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
+public class ProductsController(IUnitOfWork unit) : BaseApiController
 {
 
     [HttpGet]
@@ -15,13 +15,13 @@ public class ProductsController(IGenericRepository<Product> repo) : BaseApiContr
     {
         var spec = new ProductSpecification(specParams);
 
-        return await CreatePagedResult(repo, spec, specParams.PageIndex, specParams.PageSize);
+        return await CreatePagedResult(unit.Repository<Product>(), spec, specParams.PageIndex, specParams.PageSize);
     }
 
     [HttpGet("{id:int}")] // api/products/2
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var products = await repo.GetByIdAsync(id);
+        var products = await unit.Repository<Product>().GetByIdAsync(id);
 
         if(products == null) return NotFound();
 
@@ -32,9 +32,9 @@ public class ProductsController(IGenericRepository<Product> repo) : BaseApiContr
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product)
     {
-        repo.Add(product);
+        unit.Repository<Product>().Add(product);
 
-        if (await repo.SaveAllAsync())
+        if (await unit.Complete())
         {
             return CreatedAtAction("GetProduct", new {id = product.Id}, product);
         }
@@ -47,9 +47,9 @@ public class ProductsController(IGenericRepository<Product> repo) : BaseApiContr
         if (product.Id != id || !ProductExists(id))
             return BadRequest("Cannot update the product");
 
-        repo.Update(product);
+        unit.Repository<Product>().Update(product);
         
-        if (await repo.SaveAllAsync())
+        if (await unit.Complete())
         {
             return NoContent();
         }
@@ -61,12 +61,12 @@ public class ProductsController(IGenericRepository<Product> repo) : BaseApiContr
 
     public async Task<ActionResult> DeleteProduct(int id)
     {
-        var product = await repo.GetByIdAsync(id);
+        var product = await unit.Repository<Product>().GetByIdAsync(id);
 
         if (product == null) return NotFound();
 
-        repo.Remove(product);
-        if (await repo.SaveAllAsync())
+        unit.Repository<Product>().Remove(product);
+        if (await unit.Complete())
         {
             return NoContent();
         }
@@ -79,17 +79,17 @@ public class ProductsController(IGenericRepository<Product> repo) : BaseApiContr
     public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
     {
         var spec = new BrandListSpecification();
-        return Ok(await repo.ListAsync(spec));
+        return Ok(await unit.Repository<Product>().ListAsync(spec));
     }
 
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
     {
         var spec = new TypeListSpecification();
-        return Ok(await repo.ListAsync(spec));
+        return Ok(await unit.Repository<Product>().ListAsync(spec));
     }
     private bool ProductExists(int id)
     {
-        return repo.Exists(id);
+        return unit.Repository<Product>().Exists(id);
     }
 }
